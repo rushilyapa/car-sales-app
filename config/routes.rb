a2f6980687fb76754dc753ea075015169188b72f
Rushil Yapa <rushilyapa@yahoo.com>
2020-11-10 11:41:56 +1100

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users  
   resources :vehicles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get 'vehicles/index'
-  root 'vehicles#index'
+  root to: "vehicles#index"
 end
